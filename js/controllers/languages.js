879927969c0ddb6905fa754b0ce5f9241f06d15c
Efraim PC <efraimpc89@gmail.com>
2016-01-07 23:50:01 -0700

    2ndo cambio

--- js/controllers/languages.js
+++ js/controllers/languages.js
@@ -2,4 +2,20 @@ application.controller('languages', function($scope){
 
 	$scope.myFavLanguage = 'None'; 
 
+	$scope.php = function() {
+		$scope.myFavLanguage = 'PHP';
+	};
+
+	$scope.javascript = function() {
+		$scope.myFavLanguage = 'Javascript';
+	};
+
+	$scope.cpp = function() {
+		$scope.myFavLanguage = 'C++';
+	};
+
+	$scope.java = function() {
+		$scope.myFavLanguage = 'Java';
+	};
+
 });
